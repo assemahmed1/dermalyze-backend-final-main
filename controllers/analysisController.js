93966e6370d0d5c8a277a1b73929d6884cb0f285
assemahmed1 <assem.ahmedabdelaziz@gmail.com>
2026-04-16 21:26:13 +0200

    edit 1.4

--- controllers/analysisController.js
+++ controllers/analysisController.js
@@ -31,7 +31,7 @@ async function analyzeSkin(imageBuffer) {
     console.log(`AI Analysis attempting with token: ${token.substring(0, 4)}****`);
 
     const response = await fetch(
-      "https://api-inference.huggingface.co/models/vdyas/skin-diseases-classification",
+      "https://api-inference.huggingface.co/models/Ismail-Amroune/skin-diseases-classification",
       {
         method: "POST",
         headers: {
