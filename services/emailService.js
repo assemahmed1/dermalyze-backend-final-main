86489a041b34b2b8f27884e16d20b7140776a303
assemahmed1 <assem.ahmedabdelaziz@gmail.com>
2026-04-18 00:32:09 +0200

    chore: add temporary remote smtp verification logs

--- services/emailService.js
+++ services/emailService.js
@@ -13,6 +13,9 @@ const transporter = nodemailer.createTransport({
   },
 });
 
+console.log(`[REMOTE SMTP CHECK] SMTP_HOST is defined: ${!!process.env.SMTP_HOST}`);
+console.log(`[REMOTE SMTP CHECK] SMTP_USER is defined: ${!!process.env.SMTP_USER}`);
+
 /**
  * Helper to send OTP email
  * @param {string} email - Recipient email
